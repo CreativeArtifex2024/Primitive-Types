450c591afa1a6984bc4d8aec217641417f946cca
creativeartifex <jzdesigner2020@gmail.com>
2024-01-27 22:40:58 +0600

    Numbers

--- variables.py
+++ variables.py
@@ -3,17 +3,17 @@ y = "John"
 print(x)
 print(y)
 
-x = 4       # x is of type int
+x = 4  # x is of type int
 x = "Sally"  # x is now of type str
 print(x)
 
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
+x = str(3)  # x will be '3'
+y = int(3)  # y will be 3
 z = float(3)  # z will be 3.0
 
 x = "John"
 # is the same as
-x = 'John'
+x = "John"
 
 
 a = 4
@@ -37,8 +37,8 @@ print(course_name[:3])
 # \\
 # \n
 
-print("Python \"Programming")
-print("Python \'Programming")
+print('Python "Programming')
+print("Python 'Programming")
 print("Python \\Programming")
 print("Python \nProgramming")
 
@@ -71,3 +71,30 @@ print(course.strip())
 print(course.find("pro"))
 print(course.replace("p", "j"))
 print("pro" in course)
+print("mamun" not in course)
+
+# Numbers
+x = 1  # integer
+x = 1.1  # floats
+x = 1 + 2j  # a + bi #complex number
+
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 // 5)
+print(10 % 5)  # modulus
+print(10**3)
+
+x = 10
+x += 3  # Augmented Assignment Operator
+print(x)
+
+# working with numbers
+print(round(2.9))
+print(abs(-2.9))
+
+import math  # math module
+
+print(math.ceil(2.2))
+print(math.asin(0.22))
